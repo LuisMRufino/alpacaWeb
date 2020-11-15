ddcf9932d0c8b2a213bfc434d4f03399135734ae
LuisRufino <luismrufino@gmail.com>
2020-11-15 18:19:10 -0300

    Terminando Etapa incial de porfolio

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/stingray', function(req, res, next) {
-  res.render('stingray');
+  res.render('stingray', { language: 'spa'} );
+});
+router.get('/corefit', function(req, res, next) {
+  res.render('corefit');
 });
 
+router.get('/metrix', function(req, res, next) {
+  res.render('metrix');
+});
+
+router.get('/rgb', function(req, res, next) {
+  res.render('rgb');
+});
+router.get('/lomha', function(req, res, next) {
+  res.render('lomha');
+});
+
+router.get('/cardenas', function(req, res, next) {
+  res.render('cardenas');
+});
+
+router.get('/briella', function(req, res, next) {
+  res.render('briella');
+});
+
+router.get('/eb', function(req, res, next) {
+  res.render('eb');
+});
 router.get('/', function(req, res, next) {
   res.render('index');
 });
